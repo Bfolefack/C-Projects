ed5d761c39aa1cd23d803a2f866c8e212205ebb9
Boueny Folefack <bouenyfolefack@gmail.com>
2021-09-13 12:13:43 -0500

    Several Projects
    Created: MathQuiz, NewForms, and PictureViewer.
    Deleted: FreshWindowsTest

---
+++ MathQuiz/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace MathQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(669, 370);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(663, 166);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ready?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(663, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(3, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(663, 63);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(663, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit Answer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 370);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Math Quz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MathQuiz/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        private int time;
+        private int numOne;
+        private int numTwo;
+        private string op;
+        private int score;
+        private int total;
+        Random randomizer = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            time = 60;
+            label1.Text = label1.Text = (time / 60) + ":0" + (time % 60);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(time == 0)
+            {
+                end();
+                timer1.Stop();
+                return;
+            }
+            time--;
+            if(time %60 < 10)
+                label1.Text = (time / 60) + ":0" + (time % 60);
+            else
+                label1.Text = (time / 60) + ":" + (time % 60);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                timer1.Start();
+            }
+            if (op == "sum")
+            {
+                if(textBox1.Text.Contains((numOne+numTwo) + ""))
+                {
+                    score++;
+                }
+                total++;
+            } else if (op == "diff")
+            {
+                if (textBox1.Text.Contains((numOne-numTwo) + "")){
+                    score++;
+                }
+                total++;
+            } else if (op == "prod")
+            {
+                if (textBox1.Text.Contains((numOne*numTwo) + "")){
+                    score++;
+                }
+                total++;
+            } else if (op == "solu")
+            {
+                if (textBox1.Text.Contains((numOne/numTwo) + "")){
+                    score++;
+                }
+                total++;
+            }
+            generateQuestion();
+            if (op == "sum")
+            {
+                label2.Text = numOne + " + " + numTwo;
+            }
+            else if (op == "diff")
+            {
+                label2.Text = numOne + " - " + numTwo;
+            }
+            else if (op == "prod")
+            {
+                label2.Text = numOne + " * " + numTwo;
+            }
+            else if (op == "solu")
+            {
+                label2.Text = numOne + " / " + numTwo;
+            }
+            textBox1.Text = "";
+        }
+
+        private void generateQuestion()
+        {
+            int temp = (int)(randomizer.Next(4));
+            if(temp == 0)
+            {
+                op = "sum";
+                numOne = (int)(randomizer.Next(31));
+                numTwo = (int)(randomizer.Next(31));
+            }
+            else if(temp == 1)
+            {
+                op = "diff";
+                numOne = (int)(randomizer.Next(31));
+                numTwo = (int)(randomizer.Next(31));
+            } 
+            else if(temp == 2)
+            {
+                op = "prod";
+                numOne = (int)(randomizer.Next(13));
+                numTwo = (int)(randomizer.Next(13));
+            } 
+            else if(temp == 3)
+            {
+                op = "solu";
+                numOne = (int)(randomizer.Next(10) + 1);
+                List<int> factors = new List<int>();
+                for(int i = 1; i <= numOne; i++)
+                {
+                    if(numOne % i == 0)
+                    {
+                        factors.Add(i);
+                    }
+                }
+                Console.WriteLine(factors);
+                numTwo = factors[(int)randomizer.Next(factors.Count)];
+            }
+        }
+
+        private void end()
+        {
+            label2.Text = "Your Score: " + score + "/" + total;
+        }
+    }
+}
